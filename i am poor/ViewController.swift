ad62e1b713979d511766bb501290c0cea5a15d7e
ailingranados <ailin.granadoscn@uanl.edu.mx>
2025-08-06 09:49:45 -0600

    Initial Commit

---
+++ i am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am poor
+//
+//  Created by Ailin Elizabeth Granados Cantú on 06/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
